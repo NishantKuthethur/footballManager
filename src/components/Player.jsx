e9fd7cb19b7dadd3bc98b25b390bc5a85a2be380
Nishant Kuthethur <nishant.kuthethur@gmail.com>
2023-12-07 11:30:45 +0530

    style: fixed selected player style

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,7 +8,7 @@ const Player = ({ player, position }) => {
     }
     console.log(player.id);
     console.log(selectedPlayer);
-    
+
     if(selectedPlayer == null && player.id == 1){
       setSelectedPlayer(player);
     }
@@ -23,7 +23,7 @@ const Player = ({ player, position }) => {
   
     return (
       <div style={style} className="player-circle flex flex-col items-center justify-center gap-1 text-texts-heading" onClick={handlePlayerSelection}>
-        <div className={`relative h-8 w-8  rounded-full ${isSelected ? 'bg-primary-orange' : 'bg-neutral-2 border-2'} bg-neutral-2 border-2`}><div className="jersey-number flex justify-center text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 right-0.5">{player["Jersey Number"]}</div></div>
+        <div className={`relative h-8 w-8  rounded-full ${isSelected ? 'bg-primary-orange' : 'bg-neutral-2 border-2'}`}><div className="jersey-number flex justify-center text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 right-0.5">{player["Jersey Number"]}</div></div>
         <div className="player-name text-[14px] leading-[21px] text-center font-medium ">{player["Player Name"]}</div>
       </div>
     );
